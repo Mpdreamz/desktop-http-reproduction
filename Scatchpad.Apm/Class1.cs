6b95f8b102e1700d0cba40fe2dcb2e542f9966b8
Martijn Laarman <Mpdreamz@gmail.com>
2023-06-20 19:20:58 +0200

    code

---
+++ Scatchpad.Apm/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+
+using var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("http://localhost:5000");
+
+var activitySource = new ActivitySource("temp");
+var listener = new HttpClientGlobalListener();
+using var subscription = DiagnosticListener.AllListeners.Subscribe(listener);
+
+
+using (var activity = activitySource.StartActivity("first request", ActivityKind.Client))
+{
+    //Request with Request-Id header
+    var request = new HttpRequestMessage(HttpMethod.Get, "/"); 
+    request.Headers.Add("Request-Id", "Hello");
+    var response = httpClient.SendAsync(request).Result;
+    response.EnsureSuccessStatusCode();
+    var res = response.Content.ReadAsStringAsync().Result;
+    Console.WriteLine(res);
+}
+
+
+using (var activity2 = activitySource.StartActivity("second request", ActivityKind.Client))
+{
+    //Request without Request-Id header
+    var request = new HttpRequestMessage(HttpMethod.Get, "/"); 
+    var response = httpClient.SendAsync(request).Result;
+    response.EnsureSuccessStatusCode();
+    var res = response.Content.ReadAsStringAsync().Result;
+    Console.WriteLine(res);
+}
+
+
+
+public class HttpClientGlobalListener : IObserver<DiagnosticListener>
+{
+    private readonly HttpClientInterceptor _interceptor = new HttpClientInterceptor();
+
+    public void OnCompleted() { }
+
+    public void OnError(Exception error) { }
+
+    public void OnNext(DiagnosticListener listener)
+    {
+        Console.WriteLine(listener.Name);
+        listener.Subscribe(_interceptor);
+    }
+}
+
+public class HttpClientInterceptor : IObserver<KeyValuePair<string, object>>
+{
+    public void OnCompleted() { }
+
+    public void OnError(Exception error) { }
+
+    public void OnNext(KeyValuePair<string, object> value)
+    {
+        Console.WriteLine(value.Key);
+        Console.WriteLine(value.Value);
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+// Configure and create HttpClient mock
+var builder = WebApplication.CreateBuilder();
+builder.Logging.ClearProviders();
+builder.WebHost.UseKestrel();
+await using var application = builder.Build();
+
+application.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync(new string('-', 20));
+    await context.Response.WriteAsync("\n");
+    foreach (var (k, v) in context.Request.Headers)
+        await context.Response.WriteAsync($"{k}: {v}\n");
+});
+
+await application.RunAsync();
